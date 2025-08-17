7abdf87dfde8b10ffb577dadb5636223488bb0ac
Somnath Choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2025-08-17 16:19:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const prices = document.querySelectorAll(".price")
+  let totalPrice = 0;
+
+  prices.forEach(cell => {
+    totalPrice += parseFloat(cell.textContent) || 0
+  })
+
+  const oldTotal = document.getElementById("totalRow");
+  if (oldTotal) oldTotal.remove();
+
+  const table = document.querySelector("table")
+  const totalRow = document.createElement("tr")
+  totalRow.id = "totalRow"
+
+  const totalCell = document.createElement("td")
+  totalCell.textContent = `Total Price: Rs. ${totalPrice}`
+
+  totalRow.appendChild(totalCell)
+  table.appendChild(totalRow)
 };
 
 getSumBtn.addEventListener("click", getSum);
